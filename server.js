4dbe613e1d7271f329bff5cd87f3bc356968b4b1
soopakidd <164962769+soopakidd@users.noreply.github.com>
2026-09-23 21:04:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const ytdlp = require('yt-dlp-exec');
+const ytdlp = require('youtube-dl-exec');
 const path = require('path');
 
 const app = express();
